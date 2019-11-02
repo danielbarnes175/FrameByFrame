cfb5f6436a637cdc791001fbadef0bce1ae34f02
Daniel Barnes <31489642+danielbarnes175@users.noreply.github.com>
2019-11-02 11:52:59 -0500

    Commit Export folder

---
+++ FrameByFrame/src/Engine/Export/SaveData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using FrameByFrame.src.Engine.Animation;
+
+namespace FrameByFrame.src.Engine.Export
+{
+    public class SaveData
+    {
+        [XmlArray("Frame")]
+        [XmlArrayItem("Layer")]
+        public List<Frame> frames;
+    }
+}
---
+++ FrameByFrame/src/Engine/Export/SaveService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using FrameByFrame.src.Engine.Animation;
+
+namespace FrameByFrame.src.Engine.Export
+{
+    public class SaveService
+    {
+
+        public static void SaveAnimation(List<Frame> givenFrames)
+        {
+            SaveData saveData = new SaveData();
+            saveData.frames = givenFrames;
+            string filename = "test.fbfa";
+
+            
+        }
+    }
+}
