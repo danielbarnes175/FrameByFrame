4a3911173f034a781773b801323a5ff7f89f1b1c
Daniel Barnes <danielbarnes175@gmail.com>
2021-12-11 22:59:29 -0800

    Add support for custom text colors for buttons

--- FrameByFrame/src/UI/Components/Buttons/RedirectButton.cs
+++ FrameByFrame/src/UI/Components/Buttons/RedirectButton.cs
@@ -12,18 +12,21 @@ namespace FrameByFrame.src.UI.Components.Buttons
     {
         public string target;
         public string text;
+        public Color? textColor;
 
-        public RedirectButton(string target, string path, Vector2 position, Vector2 dimensions, string text = null) : base(path, position, dimensions)
+        public RedirectButton(string target, string path, Vector2 position, Vector2 dimensions, string text = null, Color? textColor = null) : base(path, position, dimensions)
         {
             this.target = target;
             this.text = text;
+            this.textColor = (textColor is null) ? Color.Black : textColor;
             isBeingMousedOver = false;
         }
 
-        public RedirectButton(string target, Texture2D texture, Vector2 position, Vector2 dimensions, string text = null) : base(texture, position, dimensions)
+        public RedirectButton(string target, Texture2D texture, Vector2 position, Vector2 dimensions, string text = null, Color? textColor = null) : base(texture, position, dimensions)
         {
             this.target = target;
             this.text = text;
+            this.textColor = (textColor is null) ? Color.Black : textColor;
             isBeingMousedOver = false;
         }
 
@@ -45,6 +48,7 @@ namespace FrameByFrame.src.UI.Components.Buttons
         public override void Draw(Vector2 offset)
         {
             Color color = isBeingMousedOver ? (Color.Gray * 0.5f) : Color.White;
+            Color textColorAdjusted = (Color)(isBeingMousedOver ? (textColor * 0.5f) : textColor);
             if (texture != null)
             {
                 GlobalParameters.GlobalSpriteBatch.Draw(texture,
@@ -55,13 +59,15 @@ namespace FrameByFrame.src.UI.Components.Buttons
 
             if (!string.IsNullOrEmpty(text))
             {
-                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + (dimensions.X / 2), position.Y + (dimensions.Y / 2)), Color.Black);
+                Vector2 stringSize = GlobalParameters.font.MeasureString(text);
+                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + ((dimensions.X - stringSize.X) / 2), position.Y + ((dimensions.Y - stringSize.Y) / 2)), textColorAdjusted);
             }
         }
 
         public override void Draw(Vector2 offset, float opacity)
         {
             Color color = isBeingMousedOver ? (Color.Gray * 0.5f) : Color.White;
+            Color textColorAdjusted = (Color)(isBeingMousedOver ? (textColor * 0.5f) : textColor);
             if (texture != null)
             {
                 GlobalParameters.GlobalSpriteBatch.Draw(texture,
@@ -72,13 +78,15 @@ namespace FrameByFrame.src.UI.Components.Buttons
 
             if (!string.IsNullOrEmpty(text))
             {
-                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + (dimensions.X / 2), position.Y + (dimensions.Y / 2)), Color.Black);
+                Vector2 stringSize = GlobalParameters.font.MeasureString(text);
+                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + ((dimensions.X - stringSize.X) / 2), position.Y + ((dimensions.Y - stringSize.Y) / 2)), textColorAdjusted);
             }
         }
 
         public override void Draw(Vector2 offset, Vector2 origin)
         {
             Color color = isBeingMousedOver ? new Color(255, 255, 255, 0.8f) : Color.White;
+            Color textColorAdjusted = (Color)(isBeingMousedOver ? (textColor * 0.5f) : textColor);
             if (texture != null)
             {
                 GlobalParameters.GlobalSpriteBatch.Draw(texture,
@@ -90,7 +98,7 @@ namespace FrameByFrame.src.UI.Components.Buttons
             if (!string.IsNullOrEmpty(text))
             {
                 Vector2 stringSize = GlobalParameters.font.MeasureString(text);
-                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + ((dimensions.X - stringSize.X) / 2), position.Y + ((dimensions.Y - stringSize.Y) / 2)), Color.Black);
+                GlobalParameters.GlobalSpriteBatch.DrawString(GlobalParameters.font, text, new Vector2(position.X + ((dimensions.X - stringSize.X) / 2), position.Y + ((dimensions.Y - stringSize.Y) / 2)), textColorAdjusted);
             }
         }
     }
